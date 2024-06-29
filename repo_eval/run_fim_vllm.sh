1b446ee33052dc17edce0bca38441d97440a612d
Di Wu <xiaowu200031@gmail.com>
2024-06-29 10:40:56 -0700

    eval

--- repo_eval/run_fim_vllm.sh
+++ repo_eval/run_fim_vllm.sh
@@ -13,8 +13,11 @@ data_root=`realpath ./processed_data`
 mkdir -p ${HOME_DIR}/results/repoeval
 output_root=`realpath ${HOME_DIR}/results/repoeval`
 
+# Currently, the vllm inference code only supports fim models.
+# You may refer to eval_hf.py to change the preprocessing for non-fim models
 declare -A py_model_zoo
 py_model_zoo["starcoder"]="bigcode/starcoder"
+py_model_zoo["starcoder"]="bigcode/starcoderbase"
 py_model_zoo["starcoderbase-7b"]="bigcode/starcoderbase-7b"
 py_model_zoo["starcoderbase-3b"]="bigcode/starcoderbase-3b"
 py_model_zoo["starcoderbase-1b"]="bigcode/starcoderbase-1b"
@@ -70,9 +73,6 @@ function run() {
     if [[ $task == "function_completion" ]]; then
         gen_length=256
     fi
-
-    # nvidia-smi | grep 'python' | awk '{ print $5 }' | xargs -n1 sudo kill -9
-    # ps -fA | grep 'python3' | awk '{ print $2 }' | xargs -n1 sudo kill -9
 
     python eval_vllm.py \
         --task $task \
